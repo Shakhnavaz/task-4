d3fcaf47f9679290e427bf2a01601a01166ac78b
Shakhnavaz <shakhnavaz@gmail.com>
2026-01-11 10:49:45 +0300

    changed port var for render

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
@@ -36,4 +37,5 @@ def result4():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
